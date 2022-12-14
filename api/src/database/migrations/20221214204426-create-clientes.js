18e09b31cebb56ef0d288e4c4f53e1e02c6d4ea8
Natã Elienai <nata.elienai@gmail.com>
2022-12-14 18:17:12 -0300

    feat(api): create clientes table migration

---
+++ api/src/database/migrations/20221214204426-create-clientes.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('clientes', {
+      cpfCliente: {
+        type: Sequelize.STRING(11),
+        primaryKey: true,
+        field: 'cpf_cliente',
+      },
+      nomeCliente: {
+        type: Sequelize.STRING(150),
+        field: 'nome_cliente',
+      },
+      emailCliente: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'email_cliente'
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('clientes');
+  }
+};
