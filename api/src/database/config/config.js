3e7c2d304810c4eae420da557dce9ce08902137f
Natã Elienai <nata.elienai@gmail.com>
2022-12-14 17:36:44 -0300

    refactor(api): change sequelize config to use env variables

--- api/src/database/config/config.js
+++ api/src/database/config/config.js
@@ -1,23 +1,32 @@
+require('dotenv').config();
+
+const environment = process.env.NODE_ENV || 'test';
+
+const suffix = {
+  prod: '',
+  production: '',
+  dev: '_dev',
+  development: '_dev',
+  test: '_test',
+};
+
+const options = {
+  username: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: `${process.env.MYSQL_DB_NAME || 'database'}${suffix[environment] || suffix.test}`,
+  host: process.env.MYSQL_HOST || '127.0.0.1',
+  port: process.env.MYSQL_PORT || '3306',
+  dialect: 'mysql'
+};
+
 module.exports = {
   development: {
-    username: "root",
-    password: null,
-    database: "database_development",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    ...options,
   },
   test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    ...options,
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql"
-  }
-}
+    ...options,
+  },
+};
