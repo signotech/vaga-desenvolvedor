528d8a22464f79accd0c0fde9a46907a10cabf56
Natã Elienai <nata.elienai@gmail.com>
2022-12-14 20:01:49 -0300

    feat(api): create produtos table migration

---
+++ api/src/database/migrations/20221214225446-create-produtos.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('produtos', {
+      skuProduto: {
+        type: Sequelize.STRING(100),
+        primaryKey: true,
+        field: 'sku_produto',
+      },
+      tituloProduto: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        field: 'titulo_produto',
+      },
+      preco: {
+        type: Sequelize.DECIMAL(10, 2),
+      },
+      estoque: {
+        type: Sequelize.INTEGER,
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('produtos');
+  }
+};
