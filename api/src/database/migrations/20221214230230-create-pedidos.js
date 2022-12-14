5e77ba922ccb810fbd9cb721ce4d974eb60ce475
Natã Elienai <nata.elienai@gmail.com>
2022-12-14 20:45:50 -0300

    feat(api): create pedidos table migration

---
+++ api/src/database/migrations/20221214230230-create-pedidos.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('pedidos', {
+      codigoPedido: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        field: 'codigo_pedido',
+      },
+      status: {
+        type: Sequelize.STRING(100),
+      },
+      dataPedido: {
+        type: Sequelize.DATE,
+        field: 'data_pedido'
+      },
+      cpfCliente: {
+        type: Sequelize.CHAR(11),
+        references: {
+          model: 'clientes',
+          key: 'cpf_cliente'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        field: 'cpf_cliente',
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('pedidos');
+  }
+};
