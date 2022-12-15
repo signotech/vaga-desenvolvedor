ddc30d03c09ee484f1652838276fcae3c3c93225
Natã Elienai <nata.elienai@gmail.com>
2022-12-15 00:18:45 -0300

    feat(api): add pedido model

---
+++ api/src/database/models/pedido.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+  const Pedido = sequelize.define('Pedido', {
+    codigoPedido: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    status: DataTypes.STRING(100),
+    dataPedido: DataTypes.DATE,
+    cpfCliente: DataTypes.CHAR(11),
+  }, {
+    tableName: 'pedidos',
+    timestamps: false,
+    underscored: true,
+  });
+
+  Pedido.associate = (models) => {
+    Pedido.belongsTo(models.Cliente, {
+      foreignKey: 'cpfCliente',
+      as: 'cliente',
+    });
+  };
+
+  return Pedido;
+};
