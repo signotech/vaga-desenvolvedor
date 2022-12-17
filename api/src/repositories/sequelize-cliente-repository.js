cff6e8903e13f14059d229ac37038c9a360b8a48
Natã Elienai <nata.elienai@gmail.com>
2022-12-17 16:38:29 -0300

    feat(api): add the sequelize cliente repository

---
+++ api/src/repositories/sequelize-cliente-repository.js
@@ -0,0 +1,14 @@
+const { Cliente } = require('../database/models');
+
+class SequelizeClienteRepository {
+  async create(cliente) {
+    return Cliente.create(cliente);
+  }
+
+  async existsByCpf(cpf) {
+    const cliente = await Cliente.findByPk(cpf);
+    return cliente === null ? false : true;
+  }
+}
+
+module.exports = SequelizeClienteRepository;
