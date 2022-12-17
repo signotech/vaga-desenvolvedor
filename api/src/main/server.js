4acb66a85ee3fff477bc8fc25b7cc1fb0a4a7832
Natã Elienai <nata.elienai@gmail.com>
2022-12-17 17:26:51 -0300

    style: disable eslint no-console rule in server startup

--- api/src/main/server.js
+++ api/src/main/server.js
@@ -1,4 +1,5 @@
 const app = require('./app');
 
 const PORT = process.env.API_PORT || 3001;
+// eslint-disable-next-line no-console
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
