06cb449512e0803bcd2f84125d2548ef37e96623
Natã Elienai <nata.elienai@gmail.com>
2022-12-17 16:49:03 -0300

    feat(api): add error handler middleware

---
+++ api/src/middlewares/error.js
@@ -0,0 +1,17 @@
+const ResourceAlreadyExistsError = require("../errors/resource-already-exists-error");
+const ValidationError = require("../errors/validation-error");
+const { serverError, badRequest, conflict } = require("../util/http");
+
+class ErrorMiddleware {
+  handle(error) {
+    if (error instanceof ValidationError) {
+      return badRequest({ mensagem: error.message })
+    }
+    if (error instanceof ResourceAlreadyExistsError) {
+      return conflict({ mensagem: error.message })
+    }
+    return serverError({ mensagem: 'Ocorreu um erro inesperado' });
+  }
+}
+
+module.exports = ErrorMiddleware;
--- api/src/util/http.js
+++ api/src/util/http.js
@@ -2,5 +2,20 @@ module.exports = {
   created: (data) => ({
     statusCode: 201,
     body: data
+  }),
+
+  badRequest: (error) => ({
+    statusCode: 400,
+    body: error
+  }),
+
+  conflict: (error) => ({
+    statusCode: 409,
+    body: error
+  }),
+
+  serverError: (error) => ({
+    statusCode: 500,
+    body: error
   })
 };
