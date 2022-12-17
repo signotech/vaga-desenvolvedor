d53c7f65fc9cdd2783e9ae8311c8da7219cc7c5b
Natã Elienai <nata.elienai@gmail.com>
2022-12-17 17:23:25 -0300

    style: disable eslint no-unused-vars in error middleware adapter

--- api/src/main/adapters/express-error-middleware-adapter.js
+++ api/src/main/adapters/express-error-middleware-adapter.js
@@ -1,4 +1,5 @@
 function adaptErrorMiddleware(errorMiddleware) {
+  // eslint-disable-next-line no-unused-vars
   return (err, req, res, next) => {
     const httpResponse = errorMiddleware.handle(err);
     res.status(httpResponse.statusCode).json(httpResponse.body);
