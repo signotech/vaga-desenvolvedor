b4dfcafae15f140865e3e03a72787167b3f6f485
Natã Elienai <nata.elienai@gmail.com>
2022-12-17 20:54:09 -0300

    fix(api): add validation to get all clientes use case

--- api/src/use-cases/get-all-clientes.js
+++ api/src/use-cases/get-all-clientes.js
@@ -11,9 +11,17 @@ class GetAllClientes {
     nomeCliente = '',
     emailCliente = '',
   } = {}) {
+    const parsedPorPagina = Number(porPagina);
+    const parsedPagina = Number(pagina);
+    if (!Number.isInteger(parsedPorPagina) || parsedPorPagina < 1
+        || !Number.isInteger(parsedPagina) || parsedPagina < 1
+        || !['cpfCliente', 'nomeCliente', 'emailCliente'].includes(ordenarPor)
+        || !['asc', 'desc'].includes(ordem.toLowerCase())) {
+      return [];
+    }
     return this.clienteRepository.getAll({
-      porPagina,
-      pagina,
+      porPagina: parsedPorPagina,
+      pagina: parsedPagina,
       ordenarPor,
       ordem,
       nomeCliente,
