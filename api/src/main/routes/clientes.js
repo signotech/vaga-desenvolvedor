80ec451140152498c72c9adcaf212a422667d390
Natã Elienai <nata.elienai@gmail.com>
2022-12-17 19:41:27 -0300

    feat(api): setup get all clientes route

--- api/src/main/routes/clientes.js
+++ api/src/main/routes/clientes.js
@@ -1,8 +1,10 @@
 const { adaptController } = require('../adapters/express-controller-adapter');
 const { makeCreateClienteController } = require('../factories/create-cliente-controller');
+const { makeGetAllClientesController } = require('../factories/get-all-clientes-controller');
 
 function setupClientesRoute(app) {
   app.post('/clientes', adaptController(makeCreateClienteController()));
+  app.get('/clientes', adaptController(makeGetAllClientesController()));
 }
 
 module.exports = { setupClientesRoute };
