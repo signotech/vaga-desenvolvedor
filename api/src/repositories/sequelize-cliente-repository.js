5448afb1f94012e62f1781ec9743eedc9f6132b8
Natã Elienai <nata.elienai@gmail.com>
2022-12-17 19:25:51 -0300

    feat(api): implement sequelize cliente repository getAll method

--- api/src/repositories/sequelize-cliente-repository.js
+++ api/src/repositories/sequelize-cliente-repository.js
@@ -1,13 +1,39 @@
-const { Cliente } = require('../database/models');
+const { Op } = require('sequelize');
+const { Cliente: ClienteModel } = require('../database/models');
+const Cliente = require('../entities/cliente');
 
 class SequelizeClienteRepository {
   static async create(cliente) {
-    return Cliente.create(cliente);
+    ClienteModel.create(cliente);
   }
 
   static async existsByCpf(cpf) {
-    const cliente = await Cliente.findByPk(cpf);
+    const cliente = await ClienteModel.findByPk(cpf);
     return cliente !== null;
+  }
+
+  static async getAll({
+    porPagina,
+    pagina,
+    ordenarPor,
+    ordem,
+    nomeCliente,
+    emailCliente,
+  }) {
+    try {
+      const storedClientes = await ClienteModel.findAll({
+        limit: porPagina,
+        offset: porPagina * (pagina - 1),
+        order: [[ordenarPor, ordem]],
+        where: {
+          nomeCliente: { [Op.substring]: nomeCliente },
+          emailCliente: { [Op.substring]: emailCliente },
+        },
+      });
+      return storedClientes.map((storedCliente) => new Cliente(storedCliente));
+    } catch {
+      return [];
+    }
   }
 }
 
