3505779daacb50dc4f66864c350c8aa0611b3ab1
Natã Elienai <nata.elienai@gmail.com>
2022-12-18 18:22:13 -0300

    feat(api): add getAll method to sequelize produto repository

--- api/src/repositories/sequelize-produto-repository.js
+++ api/src/repositories/sequelize-produto-repository.js
@@ -1,4 +1,6 @@
+const { Op } = require('sequelize');
 const { Produto: ProdutoModel } = require('../database/models');
+const Produto = require('../entities/produto');
 
 class SequelizeProdutoRepository {
   static async create(produto) {
@@ -8,6 +10,28 @@ class SequelizeProdutoRepository {
   static async existsBySku(sku) {
     const produto = await ProdutoModel.findByPk(sku);
     return produto !== null;
+  }
+
+  static async getAll({
+    porPagina,
+    pagina,
+    ordenarPor,
+    ordem,
+    tituloProduto,
+    preco,
+    estoque,
+  }) {
+    const storedProdutos = await ProdutoModel.findAll({
+      limit: porPagina,
+      offset: porPagina * (pagina - 1),
+      order: [[ordenarPor, ordem]],
+      where: {
+        tituloProduto: { [Op.substring]: tituloProduto },
+        ...(preco !== undefined && { preco }),
+        ...(estoque !== undefined && { estoque }),
+      },
+    });
+    return storedProdutos.map((storedProduto) => new Produto(storedProduto));
   }
 }
 
