72e044f0722137f413adc95d1c65a3bf64bfef66
Natã Elienai <nata.elienai@gmail.com>
2022-12-17 23:01:49 -0300

    feat(api): add UpdateCliente use case

---
+++ api/src/use-cases/update-cliente.js
@@ -0,0 +1,20 @@
+const Cliente = require('../entities/cliente');
+const ResourceNotFoundError = require('../errors/resource-not-found-error');
+
+class UpdateCliente {
+  constructor(clienteRepository) {
+    this.clienteRepository = clienteRepository;
+  }
+
+  async execute({ cpfCliente, nomeCliente, emailCliente }) {
+    const cpfExiste = await this.clienteRepository.existsByCpf(cpfCliente);
+    if (!cpfExiste) {
+      throw new ResourceNotFoundError('Não foi encontrado um cliente com o CPF informado');
+    }
+    const cliente = new Cliente({ cpfCliente, nomeCliente, emailCliente });
+    await this.clienteRepository.update(cliente);
+    return cliente;
+  }
+}
+
+module.exports = UpdateCliente;
