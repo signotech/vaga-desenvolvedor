9951cbf0debdd9baeb644896e930a15b9e4b4fe2
Natã Elienai <nata.elienai@gmail.com>
2022-12-18 20:05:00 -0300

    feat(api): add delete produto by sku use case

---
+++ api/src/use-cases/delete-produto-by-sku-use-case.js
@@ -0,0 +1,17 @@
+const ResourceNotFoundError = require('../errors/resource-not-found-error');
+
+class DeleteProdutoBySkuUseCase {
+  constructor(produtoRepository) {
+    this.produtoRepository = produtoRepository;
+  }
+
+  async execute({ skuProduto }) {
+    const skuExists = await this.produtoRepository.existsBySku(skuProduto);
+    if (!skuExists) {
+      throw new ResourceNotFoundError('Não foi encontrado um produto com o SKU informado');
+    }
+    await this.produtoRepository.deleteBySku(skuProduto);
+  }
+}
+
+module.exports = DeleteProdutoBySkuUseCase;
