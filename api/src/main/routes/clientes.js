ed64acf9b351238222b1beb57fd3bace6a441390
Natã Elienai <nata.elienai@gmail.com>
2022-12-17 22:51:44 -0300

    feat(api): setup get cliente by cpf route

--- api/src/main/routes/clientes.js
+++ api/src/main/routes/clientes.js
@@ -1,10 +1,12 @@
 const { adaptController } = require('../adapters/express-controller-adapter');
 const { makeCreateClienteController } = require('../factories/create-cliente-controller');
 const { makeGetAllClientesController } = require('../factories/get-all-clientes-controller');
+const { makeGetClienteByCpfController } = require('../factories/get-cliente-by-cpf-controller');
 
 function setupClientesRoute(app) {
   app.post('/clientes', adaptController(makeCreateClienteController()));
   app.get('/clientes', adaptController(makeGetAllClientesController()));
+  app.get('/clientes/:cpfCliente', adaptController(makeGetClienteByCpfController()));
 }
 
 module.exports = { setupClientesRoute };
