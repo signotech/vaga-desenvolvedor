47e2931290389211ddf9a548d35922319e0fa57d
Natã Elienai <nata.elienai@gmail.com>
2022-12-18 16:39:12 -0300

    feat(api): add create produto use case

---
+++ api/src/use-cases/create-produto-use-case.js
@@ -0,0 +1,30 @@
+const Produto = require('../entities/produto');
+const ResourceAlreadyExistsError = require('../errors/resource-already-exists-error');
+
+class CreateProdutoUseCase {
+  constructor(produtoRepository) {
+    this.produtoRepository = produtoRepository;
+  }
+
+  async execute({
+    skuProduto,
+    tituloProduto,
+    preco,
+    estoque,
+  }) {
+    const produto = new Produto({
+      skuProduto,
+      tituloProduto,
+      preco,
+      estoque,
+    });
+    const skuExists = await this.produtoRepository.existsBySku(skuProduto);
+    if (skuExists) {
+      throw new ResourceAlreadyExistsError('O SKU informado já existe');
+    }
+    await this.produtoRepository.create(produto);
+    return produto;
+  }
+}
+
+module.exports = CreateProdutoUseCase;
