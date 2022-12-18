e01279c06315a4c9dc773fc6a770e680511d90f9
Natã Elienai <nata.elienai@gmail.com>
2022-12-18 18:35:37 -0300

    feat(api): setup get produto by sku route

--- api/src/main/routes/produtos.js
+++ api/src/main/routes/produtos.js
@@ -1,10 +1,12 @@
 const { adaptController } = require('../adapters/express-controller-adapter');
 const { makeCreateProdutoController } = require('../factories/create-produto-controller-factory');
 const { makeGetAllProdutosController } = require('../factories/get-all-produtos-controller-factory');
+const { makeGetProdutoBySkuController } = require('../factories/get-produto-by-sku-controller-factory');
 
 function setupProdutosRoute(app) {
   app.post('/produtos', adaptController(makeCreateProdutoController()));
   app.get('/produtos', adaptController(makeGetAllProdutosController()));
+  app.get('/produtos/:skuProduto', adaptController(makeGetProdutoBySkuController()));
 }
 
 module.exports = { setupProdutosRoute };
