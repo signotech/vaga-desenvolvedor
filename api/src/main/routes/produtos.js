4850b3c0ae025cf49ff5089437ce45c657a27b19
Natã Elienai <nata.elienai@gmail.com>
2022-12-18 19:01:29 -0300

    feat(api): setup update produto route

--- api/src/main/routes/produtos.js
+++ api/src/main/routes/produtos.js
@@ -2,11 +2,13 @@ const { adaptController } = require('../adapters/express-controller-adapter');
 const { makeCreateProdutoController } = require('../factories/create-produto-controller-factory');
 const { makeGetAllProdutosController } = require('../factories/get-all-produtos-controller-factory');
 const { makeGetProdutoBySkuController } = require('../factories/get-produto-by-sku-controller-factory');
+const { makeUpdateProdutoController } = require('../factories/update-produto-controller-factory');
 
 function setupProdutosRoute(app) {
   app.post('/produtos', adaptController(makeCreateProdutoController()));
   app.get('/produtos', adaptController(makeGetAllProdutosController()));
   app.get('/produtos/:skuProduto', adaptController(makeGetProdutoBySkuController()));
+  app.put('/produtos/:skuProduto', adaptController(makeUpdateProdutoController()));
 }
 
 module.exports = { setupProdutosRoute };
