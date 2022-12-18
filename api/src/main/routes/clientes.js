5d7798040325d3be1d36fe51ad45f4f7e7be9784
Natã Elienai <nata.elienai@gmail.com>
2022-12-18 13:52:08 -0300

    feat(api): setup delete cliente by cpf route

--- api/src/main/routes/clientes.js
+++ api/src/main/routes/clientes.js
@@ -1,6 +1,7 @@
 const { adaptController } = require('../adapters/express-controller-adapter');
 const { makeCreateClienteController } = require('../factories/create-cliente-controller');
 const { makeDeleteAllClientesController } = require('../factories/delete-all-clientes-controller');
+const { makeDeleteClienteByCpfController } = require('../factories/delete-cliente-by-cpf-controller');
 const { makeGetAllClientesController } = require('../factories/get-all-clientes-controller');
 const { makeGetClienteByCpfController } = require('../factories/get-cliente-by-cpf-controller');
 const { makeUpdateClienteController } = require('../factories/update-cliente-controller');
@@ -11,6 +12,7 @@ function setupClientesRoute(app) {
   app.get('/clientes/:cpfCliente', adaptController(makeGetClienteByCpfController()));
   app.put('/clientes/:cpfCliente', adaptController(makeUpdateClienteController()));
   app.delete('/clientes', adaptController(makeDeleteAllClientesController()));
+  app.delete('/clientes/:cpfCliente', adaptController(makeDeleteClienteByCpfController()));
 }
 
 module.exports = { setupClientesRoute };
