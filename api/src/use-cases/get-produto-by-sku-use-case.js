3c342cc188360fb24278f7f6106dec1ad2eb5a4a
Natã Elienai <nata.elienai@gmail.com>
2022-12-18 18:34:05 -0300

    feat(api): add get produto by sku use case

---
+++ api/src/use-cases/get-produto-by-sku-use-case.js
@@ -0,0 +1,17 @@
+const ResourceNotFoundError = require('../errors/resource-not-found-error');
+
+class GetProdutoBySkuUseCase {
+  constructor(produtoRepository) {
+    this.produtoRepository = produtoRepository;
+  }
+
+  async execute({ skuProduto }) {
+    const produto = await this.produtoRepository.getBySku(skuProduto);
+    if (produto === null) {
+      throw new ResourceNotFoundError('Não foi encontrado um produto com o SKU informado');
+    }
+    return produto;
+  }
+}
+
+module.exports = GetProdutoBySkuUseCase;
