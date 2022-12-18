876fe80e5c51f7244016a12e309509fb95a6d3de
Natã Elienai <nata.elienai@gmail.com>
2022-12-18 13:43:33 -0300

    feat(api): add delete cliente by cpf use case

---
+++ api/src/use-cases/delete-cliente-by-cpf.js
@@ -0,0 +1,17 @@
+const ResourceNotFoundError = require('../errors/resource-not-found-error');
+
+class DeleteClienteByCpf {
+  constructor(clienteRepository) {
+    this.clienteRepository = clienteRepository;
+  }
+
+  async execute({ cpfCliente }) {
+    const cpfExists = await this.clienteRepository.existsByCpf(cpfCliente);
+    if (!cpfExists) {
+      throw new ResourceNotFoundError('Não foi encontrado um cliente com o CPF informado');
+    }
+    await this.clienteRepository.deleteByCpf(cpfCliente);
+  }
+}
+
+module.exports = DeleteClienteByCpf;
