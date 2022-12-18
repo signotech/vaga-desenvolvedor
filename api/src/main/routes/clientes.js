8dd92e58d7c95787477076352726b40631c2eec9
Natã Elienai <nata.elienai@gmail.com>
2022-12-17 23:18:57 -0300

    feat(api): setup update cliente route

--- api/src/main/routes/clientes.js
+++ api/src/main/routes/clientes.js
@@ -2,11 +2,13 @@ const { adaptController } = require('../adapters/express-controller-adapter');
 const { makeCreateClienteController } = require('../factories/create-cliente-controller');
 const { makeGetAllClientesController } = require('../factories/get-all-clientes-controller');
 const { makeGetClienteByCpfController } = require('../factories/get-cliente-by-cpf-controller');
+const { makeUpdateClienteController } = require('../factories/update-cliente-controller');
 
 function setupClientesRoute(app) {
   app.post('/clientes', adaptController(makeCreateClienteController()));
   app.get('/clientes', adaptController(makeGetAllClientesController()));
   app.get('/clientes/:cpfCliente', adaptController(makeGetClienteByCpfController()));
+  app.put('/clientes/:cpfCliente', adaptController(makeUpdateClienteController()));
 }
 
 module.exports = { setupClientesRoute };
