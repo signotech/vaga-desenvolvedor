b2785e1760182a7dac7277bd60a3dff30c0318db
Natã Elienai <nata.elienai@gmail.com>
2022-12-18 18:23:24 -0300

    feat(api): setup get all produtos route

--- api/src/main/routes/produtos.js
+++ api/src/main/routes/produtos.js
@@ -1,8 +1,10 @@
 const { adaptController } = require('../adapters/express-controller-adapter');
 const { makeCreateProdutoController } = require('../factories/create-produto-controller-factory');
+const { makeGetAllProdutosController } = require('../factories/get-all-produtos-controller-factory');
 
 function setupProdutosRoute(app) {
   app.post('/produtos', adaptController(makeCreateProdutoController()));
+  app.get('/produtos', adaptController(makeGetAllProdutosController()));
 }
 
 module.exports = { setupProdutosRoute };
