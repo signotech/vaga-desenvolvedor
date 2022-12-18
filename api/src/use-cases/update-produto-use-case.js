66579781fa58640eae1b986951abbe41ef6dcdff
Natã Elienai <nata.elienai@gmail.com>
2022-12-18 19:00:15 -0300

    feat(api): add update produto use case

---
+++ api/src/use-cases/update-produto-use-case.js
@@ -0,0 +1,30 @@
+const Produto = require('../entities/produto');
+const ResourceNotFoundError = require('../errors/resource-not-found-error');
+
+class UpdateProdutoUseCase {
+  constructor(produtoRepository) {
+    this.produtoRepository = produtoRepository;
+  }
+
+  async execute({
+    skuProduto,
+    tituloProduto,
+    preco,
+    estoque,
+  }) {
+    const skuExists = await this.produtoRepository.existsBySku(skuProduto);
+    if (!skuExists) {
+      throw new ResourceNotFoundError('Não foi encontrado um produto com o SKU informado');
+    }
+    const produto = new Produto({
+      skuProduto,
+      tituloProduto,
+      preco,
+      estoque,
+    });
+    await this.produtoRepository.update(produto);
+    return produto;
+  }
+}
+
+module.exports = UpdateProdutoUseCase;
