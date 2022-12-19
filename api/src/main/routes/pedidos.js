045a781c064d85ede11119b643654c38b9ced721
Natã Elienai <nata.elienai@gmail.com>
2022-12-19 18:22:56 -0300

    feat(api): setup get all pedidos route

--- api/src/main/routes/pedidos.js
+++ api/src/main/routes/pedidos.js
@@ -1,8 +1,10 @@
 const { adaptController } = require('../adapters/express-controller-adapter');
 const { makeCreatePedidoController } = require('../factories/create-pedido-controller-factory');
+const { makeGetAllPedidosController } = require('../factories/get-all-pedidos-controller-factory');
 
 function setupPedidosRoute(app) {
   app.post('/pedidos', adaptController(makeCreatePedidoController()));
+  app.get('/pedidos', adaptController(makeGetAllPedidosController()));
 }
 
 module.exports = { setupPedidosRoute };
