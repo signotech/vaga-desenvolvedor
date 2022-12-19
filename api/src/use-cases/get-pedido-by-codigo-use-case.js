a3d05409fc908b718c0c2ce9491d283951b6fa28
Natã Elienai <nata.elienai@gmail.com>
2022-12-19 19:40:51 -0300

    feat(api): add get pedido by codigo use case

---
+++ api/src/use-cases/get-pedido-by-codigo-use-case.js
@@ -0,0 +1,17 @@
+const ResourceNotFoundError = require('../errors/resource-not-found-error');
+
+class GetPedidoByCodigoUseCase {
+  constructor(pedidoRepository) {
+    this.pedidoRepository = pedidoRepository;
+  }
+
+  async execute({ codigoPedido }) {
+    const pedido = await this.pedidoRepository.getByCodigo(codigoPedido);
+    if (pedido === null) {
+      throw new ResourceNotFoundError('Não foi encontrado um pedido com o código informado');
+    }
+    return pedido;
+  }
+}
+
+module.exports = GetPedidoByCodigoUseCase;
