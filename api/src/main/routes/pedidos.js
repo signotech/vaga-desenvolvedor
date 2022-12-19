45051814e3f2d86bd31143f1e3dff763db9b71bd
Natã Elienai <nata.elienai@gmail.com>
2022-12-19 20:36:15 -0300

    feat(api): setup update pedido route

--- api/src/main/routes/pedidos.js
+++ api/src/main/routes/pedidos.js
@@ -2,11 +2,13 @@ const { adaptController } = require('../adapters/express-controller-adapter');
 const { makeCreatePedidoController } = require('../factories/create-pedido-controller-factory');
 const { makeGetAllPedidosController } = require('../factories/get-all-pedidos-controller-factory');
 const { makeGetPedidoByCodigoController } = require('../factories/get-pedido-by-codigo-controller-factory');
+const { makeUpdatePedidoController } = require('../factories/update-pedido-controller-factory');
 
 function setupPedidosRoute(app) {
   app.post('/pedidos', adaptController(makeCreatePedidoController()));
   app.get('/pedidos', adaptController(makeGetAllPedidosController()));
   app.get('/pedidos/:codigoPedido', adaptController(makeGetPedidoByCodigoController()));
+  app.patch('/pedidos/:codigoPedido', adaptController(makeUpdatePedidoController()));
 }
 
 module.exports = { setupPedidosRoute };
