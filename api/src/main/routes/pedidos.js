fb51fb317a9f2b645278af01888adbb41934ebf2
Natã Elienai <nata.elienai@gmail.com>
2022-12-19 19:41:34 -0300

    feat(api): setup get pedido by codigo route

--- api/src/main/routes/pedidos.js
+++ api/src/main/routes/pedidos.js
@@ -1,10 +1,12 @@
 const { adaptController } = require('../adapters/express-controller-adapter');
 const { makeCreatePedidoController } = require('../factories/create-pedido-controller-factory');
 const { makeGetAllPedidosController } = require('../factories/get-all-pedidos-controller-factory');
+const { makeGetPedidoByCodigoController } = require('../factories/get-pedido-by-codigo-controller-factory');
 
 function setupPedidosRoute(app) {
   app.post('/pedidos', adaptController(makeCreatePedidoController()));
   app.get('/pedidos', adaptController(makeGetAllPedidosController()));
+  app.get('/pedidos/:codigoPedido', adaptController(makeGetPedidoByCodigoController()));
 }
 
 module.exports = { setupPedidosRoute };
