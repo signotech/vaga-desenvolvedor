c60ad91b35693f0c5ce996eea4406f502aacfd63
Natã Elienai <nata.elienai@gmail.com>
2022-12-19 20:35:39 -0300

    feat(api): add update pedido use case

---
+++ api/src/use-cases/update-pedido-use-case.js
@@ -0,0 +1,23 @@
+const Pedido = require('../entities/pedido');
+const ResourceNotFoundError = require('../errors/resource-not-found-error');
+
+class UpdateClienteUseCase {
+  constructor(pedidoRepository) {
+    this.pedidoRepository = pedidoRepository;
+  }
+
+  async execute({ codigoPedido, status }) {
+    const pedido = await this.pedidoRepository.getByCodigo(codigoPedido);
+    if (pedido === null) {
+      throw new ResourceNotFoundError('Não foi encontrado um pedido com o código informado');
+    }
+    const updatedPedido = new Pedido({
+      ...pedido,
+      status,
+    });
+    await this.pedidoRepository.update(updatedPedido);
+    return updatedPedido;
+  }
+}
+
+module.exports = UpdateClienteUseCase;
