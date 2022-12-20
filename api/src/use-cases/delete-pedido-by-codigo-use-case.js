1cfdd0ece4bf39f531c76c7df56ae222a0c3851e
Natã Elienai <nata.elienai@gmail.com>
2022-12-19 22:14:56 -0300

    feat(api): add delete pedido by codigo use case

---
+++ api/src/use-cases/delete-pedido-by-codigo-use-case.js
@@ -0,0 +1,17 @@
+const ResourceNotFoundError = require('../errors/resource-not-found-error');
+
+class DeletePedidoByCodigoUseCase {
+  constructor(pedidoRepository) {
+    this.pedidoRepository = pedidoRepository;
+  }
+
+  async execute({ codigoPedido }) {
+    const codigoExists = await this.pedidoRepository.existsByCodigo(codigoPedido);
+    if (!codigoExists) {
+      throw new ResourceNotFoundError('Não foi encontrado um pedido com o código informado');
+    }
+    await this.pedidoRepository.deleteByCodigo(codigoPedido);
+  }
+}
+
+module.exports = DeletePedidoByCodigoUseCase;
