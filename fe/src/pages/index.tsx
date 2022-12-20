6b52f5657ebca2db3712c7f86605443574b2a38f
jonas <jonasmartinsrubim@gmail.com>
2022-12-20 19:12:16 -0300

    chore: correction page

--- fe/src/pages/index.tsx
+++ fe/src/pages/index.tsx
@@ -1,7 +1,25 @@
-import { NextPage } from 'next'
+import { GetServerSideProps, NextPage } from 'next'
+import nookies from 'nookies'
 
 import { Login as LoginComponent } from '../pages-components/Login'
 
 const Login: NextPage = () => <LoginComponent />
 
 export default Login
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const cookies = nookies.get(context)
+  const { isAuthorized } = cookies
+
+  if (isAuthorized) {
+    return {
+      redirect: {
+        destination: '/products',
+        permanent: false,
+      },
+    }
+  }
+  return {
+    props: {},
+  }
+}
