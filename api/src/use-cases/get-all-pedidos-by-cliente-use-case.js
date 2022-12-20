681e82d20988a43b6e83ca2dd56fbcb02db3315e
Natã Elienai <nata.elienai@gmail.com>
2022-12-20 00:45:35 -0300

    feat(api): add get all pedidos by cliente use case

---
+++ api/src/use-cases/get-all-pedidos-by-cliente-use-case.js
@@ -0,0 +1,18 @@
+const ResourceNotFoundError = require('../errors/resource-not-found-error');
+
+class GetAllPedidosByClienteUseCase {
+  constructor(pedidoRepository, clienteRepository) {
+    this.pedidoRepository = pedidoRepository;
+    this.clienteRepository = clienteRepository;
+  }
+
+  async execute({ cpfCliente }) {
+    const cpfExists = await this.clienteRepository.existsByCpf(cpfCliente);
+    if (!cpfExists) {
+      throw new ResourceNotFoundError('Não foi encontrado um cliente com o CPF informado');
+    }
+    return this.pedidoRepository.getAllByCpfCliente(cpfCliente);
+  }
+}
+
+module.exports = GetAllPedidosByClienteUseCase;
