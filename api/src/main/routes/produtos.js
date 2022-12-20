a126ab1397543ed5c64398da741629a8a5b09a71
Natã Elienai <nata.elienai@gmail.com>
2022-12-20 00:27:29 -0300

    feat(api): setup get all produtos by pedido route

--- api/src/main/routes/produtos.js
+++ api/src/main/routes/produtos.js
@@ -2,6 +2,7 @@ const { adaptController } = require('../adapters/express-controller-adapter');
 const { makeCreateProdutoController } = require('../factories/create-produto-controller-factory');
 const { makeDeleteAllProdutosController } = require('../factories/delete-all-produtos-controller-factory');
 const { makeDeleteProdutoBySkuController } = require('../factories/delete-produto-by-sku-controller-factory');
+const { makeGetAllProdutosByPedidoController } = require('../factories/get-all-produtos-by-pedido-controller-factory');
 const { makeGetAllProdutosController } = require('../factories/get-all-produtos-controller-factory');
 const { makeGetProdutoBySkuController } = require('../factories/get-produto-by-sku-controller-factory');
 const { makeUpdateProdutoController } = require('../factories/update-produto-controller-factory');
@@ -13,6 +14,7 @@ function setupProdutosRoute(app) {
   app.put('/produtos/:skuProduto', adaptController(makeUpdateProdutoController()));
   app.delete('/produtos', adaptController(makeDeleteAllProdutosController()));
   app.delete('/produtos/:skuProduto', adaptController(makeDeleteProdutoBySkuController()));
+  app.get('/pedidos/:codigoPedido/produtos', adaptController(makeGetAllProdutosByPedidoController()));
 }
 
 module.exports = { setupProdutosRoute };
