cc4daf9be2aac1c0ff5681f768e909edb0862b3d
Natã Elienai <nata.elienai@gmail.com>
2022-12-19 22:03:29 -0300

    feat(api): setup delete all pedidos route

--- api/src/main/routes/pedidos.js
+++ api/src/main/routes/pedidos.js
@@ -1,5 +1,6 @@
 const { adaptController } = require('../adapters/express-controller-adapter');
 const { makeCreatePedidoController } = require('../factories/create-pedido-controller-factory');
+const { makeDeleteAllPedidosController } = require('../factories/delete-all-pedidos-controller-factory');
 const { makeGetAllPedidosController } = require('../factories/get-all-pedidos-controller-factory');
 const { makeGetPedidoByCodigoController } = require('../factories/get-pedido-by-codigo-controller-factory');
 const { makeUpdatePedidoController } = require('../factories/update-pedido-controller-factory');
@@ -9,6 +10,7 @@ function setupPedidosRoute(app) {
   app.get('/pedidos', adaptController(makeGetAllPedidosController()));
   app.get('/pedidos/:codigoPedido', adaptController(makeGetPedidoByCodigoController()));
   app.patch('/pedidos/:codigoPedido', adaptController(makeUpdatePedidoController()));
+  app.delete('/pedidos', adaptController(makeDeleteAllPedidosController()));
 }
 
 module.exports = { setupPedidosRoute };
