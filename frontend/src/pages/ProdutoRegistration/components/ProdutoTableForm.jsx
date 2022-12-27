6767f21cf2d43cf354aaaba4be8729f6e92d7591
Natã Elienai <nata.elienai@gmail.com>
2022-12-27 17:10:00 -0300

    feat(front): add validation to produto table form

--- frontend/src/pages/ProdutoRegistration/components/ProdutoTableForm.jsx
+++ frontend/src/pages/ProdutoRegistration/components/ProdutoTableForm.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 export default function ProdutoTableForm({
   produto,
@@ -7,60 +8,142 @@ export default function ProdutoTableForm({
   onCancel,
   skuDisabled,
 }) {
+  const [skuValidation, setSkuValidation] = useState({
+    isValid: true,
+    message: '',
+  });
+  const [tituloValidation, setTituloValidation] = useState({
+    isValid: true,
+    message: '',
+  });
+  const [precoValidation, setPrecoValidation] = useState({
+    isValid: true,
+    message: '',
+  });
+  const [estoqueValidation, setEstoqueValidation] = useState({
+    isValid: true,
+    message: '',
+  });
+
   const handleInputChange = ({ target: { name, value } }) => {
     onProdutoChange({ ...produto, [name]: value });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    onProdutoSubmit();
+    let shouldSubmit = true;
+
+    setSkuValidation({ isValid: true, message: '' });
+    setTituloValidation({ isValid: true, message: '' });
+    setPrecoValidation({ isValid: true, message: '' });
+    setEstoqueValidation({ isValid: true, message: '' });
+
+    if (!produto.skuProduto) {
+      shouldSubmit = false;
+      setSkuValidation({
+        isValid: false,
+        message: 'O SKU deve conter pelo menos 1 caractere',
+      });
+    }
+
+    if (!produto.tituloProduto) {
+      shouldSubmit = false;
+      setTituloValidation({
+        isValid: false,
+        message: 'O título do produto deve conter pelo menos 1 caractere',
+      });
+    }
+
+    const preco = Number(produto.preco);
+    if (preco <= 0) {
+      shouldSubmit = false;
+      setPrecoValidation({
+        isValid: false,
+        message: 'O preço deve ser maior que 0',
+      });
+    }
+
+    const estoque = Number(produto.estoque);
+    if (!produto.estoque || !Number.isInteger(estoque) || estoque < 0) {
+      shouldSubmit = false;
+      setEstoqueValidation({
+        isValid: false,
+        message: 'O estoque deve ser um número inteiro positivo',
+      });
+    }
+
+    if (shouldSubmit) {
+      onProdutoSubmit();
+    }
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="skuProduto">
-        SKU
-        <input
-          type="text"
-          id="skuProduto"
-          name="skuProduto"
-          value={produto.skuProduto}
-          onChange={handleInputChange}
-          disabled={skuDisabled}
-        />
-      </label>
-      <label htmlFor="tituloProduto">
-        Título
-        <input
-          type="text"
-          id="tituloProduto"
-          name="tituloProduto"
-          value={produto.tituloProduto}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label htmlFor="preco">
-        Preço
-        <input
-          type="number"
-          id="preco"
-          name="preco"
-          value={produto.preco}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label htmlFor="estoque">
-        Estoque
-        <input
-          type="number"
-          id="estoque"
-          name="estoque"
-          value={produto.estoque}
-          onChange={handleInputChange}
-        />
-      </label>
-      <button type="submit">Salvar</button>
-      <button type="button" onClick={onCancel}>Cancelar</button>
+      <div>
+        <label htmlFor="skuProduto">
+          SKU
+          <input
+            type="text"
+            id="skuProduto"
+            name="skuProduto"
+            value={produto.skuProduto}
+            onChange={handleInputChange}
+            disabled={skuDisabled}
+          />
+        </label>
+        {!skuValidation.isValid && (
+          <div>{skuValidation.message}</div>
+        )}
+      </div>
+      <div>
+        <label htmlFor="tituloProduto">
+          Título
+          <input
+            type="text"
+            id="tituloProduto"
+            name="tituloProduto"
+            value={produto.tituloProduto}
+            onChange={handleInputChange}
+          />
+        </label>
+        {!tituloValidation.isValid && (
+          <div>{tituloValidation.message}</div>
+        )}
+      </div>
+      <div>
+        <label htmlFor="preco">
+          Preço
+          <input
+            type="number"
+            id="preco"
+            name="preco"
+            value={produto.preco}
+            onChange={handleInputChange}
+          />
+        </label>
+        {!precoValidation.isValid && (
+          <div>{precoValidation.message}</div>
+        )}
+      </div>
+      <div>
+        <label htmlFor="estoque">
+          Estoque
+          <input
+            type="number"
+            id="estoque"
+            name="estoque"
+            value={produto.estoque}
+            onChange={handleInputChange}
+          />
+        </label>
+        {!estoqueValidation.isValid && (
+          <div>{estoqueValidation.message}</div>
+        )}
+      </div>
+      <div>
+        <button type="button" onClick={onCancel}>Cancelar</button>
+        <button type="submit">Salvar</button>
+      </div>
     </form>
   );
 }
