4de18190bbe3b492036ada6256efa4a863399318
Natã Elienai <nata.elienai@gmail.com>
2022-12-29 01:27:59 -0300

    feat(front): wrap cliente registration page in a bootstrap container

--- frontend/src/pages/ClienteRegistration/index.jsx
+++ frontend/src/pages/ClienteRegistration/index.jsx
@@ -1,11 +1,12 @@
+import Container from 'react-bootstrap/Container';
 import FilterablePaginatedClienteTable from './components/FilterablePaginatedClienteTable';
 import Header from '../../components/Header';
 
 export default function ClienteRegistration() {
   return (
-    <div>
+    <Container className="my-4 col-lg-8">
       <Header />
       <FilterablePaginatedClienteTable />
-    </div>
+    </Container>
   );
 }
