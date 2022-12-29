8e6105d3029745d0032b2410f1fd1b87b6efb1b4
Natã Elienai <nata.elienai@gmail.com>
2022-12-29 14:20:04 -0300

    refactor(front): style pedido table rows with bootstrap components

--- frontend/src/pages/PedidoRegistration/components/PedidoTableRow.jsx
+++ frontend/src/pages/PedidoRegistration/components/PedidoTableRow.jsx
@@ -1,4 +1,8 @@
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+import { BsTrashFill, BsPencilFill } from 'react-icons/bs';
 
 export default function PedidoTableRow({
   pedido,
@@ -6,13 +10,21 @@ export default function PedidoTableRow({
   onDeleteButtonClick,
 }) {
   return (
-    <tr>
+    <tr className="align-middle">
       <td>{pedido.codigoPedido}</td>
       <td>{pedido.status}</td>
       <td>{pedido.dataPedido.replace(/T|Z/g, ' ')}</td>
       <td>
-        <button type="button" onClick={onUpdateButtonClick}>Atualizar</button>
-        <button type="button" onClick={onDeleteButtonClick}>Deletar</button>
+        <OverlayTrigger overlay={<Tooltip>Editar</Tooltip>}>
+          <Button variant="white" type="button" onClick={onUpdateButtonClick}>
+            <BsPencilFill color="orange" />
+          </Button>
+        </OverlayTrigger>
+        <OverlayTrigger overlay={<Tooltip>Deletar</Tooltip>}>
+          <Button variant="white" type="button" onClick={onDeleteButtonClick}>
+            <BsTrashFill color="red" />
+          </Button>
+        </OverlayTrigger>
       </td>
     </tr>
   );
