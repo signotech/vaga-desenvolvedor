af7b0cc8a129d35d75adecb4b5ddd2343890ccff
Natã Elienai <nata.elienai@gmail.com>
2022-12-29 15:12:36 -0300

    feat(api): add more records to clientes and produtos seeders

--- api/src/database/seeders/20221215000646-add-clientes.js
+++ api/src/database/seeders/20221215000646-add-clientes.js
@@ -18,6 +18,36 @@ module.exports = {
         nome_cliente: 'Jane Doe',
         email_cliente: 'jane.doe@email.com',
       },
+      {
+        cpf_cliente: '12345678901',
+        nome_cliente: 'Wednesday Addams',
+        email_cliente: 'wedadd@email.com',
+      },
+      {
+        cpf_cliente: '10987654321',
+        nome_cliente: 'Charles Xavier',
+        email_cliente: 'charlie@email.com',
+      },
+      {
+        cpf_cliente: '65498732109',
+        nome_cliente: 'Wanda Maximoff',
+        email_cliente: 'wandamax@email.com',
+      },
+      {
+        cpf_cliente: '75395125846',
+        nome_cliente: 'Luke Skywalker',
+        email_cliente: 'littleluke@email.com',
+      },
+      {
+        cpf_cliente: '85236974109',
+        nome_cliente: 'Ellie Willians',
+        email_cliente: 'ellie.w@email.com',
+      },
+      {
+        cpf_cliente: '96325874197',
+        nome_cliente: 'Joel Miller',
+        email_cliente: 'miller.joel@email.com',
+      },
     ], {});
   },
 
