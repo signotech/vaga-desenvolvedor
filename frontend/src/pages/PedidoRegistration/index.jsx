27c9aeb106074c6c2238705679ff93a573468938
Natã Elienai <nata.elienai@gmail.com>
2022-12-29 13:19:43 -0300

    feat(front): wrap pedido registration page in a bootstrap container

--- frontend/src/pages/PedidoRegistration/index.jsx
+++ frontend/src/pages/PedidoRegistration/index.jsx
@@ -1,11 +1,12 @@
+import Container from 'react-bootstrap/Container';
 import Header from '../../components/Header';
 import FilterablePaginatedPedidoTable from './components/FilterablePaginatedPedidoTable';
 
 export default function PedidoRegistration() {
   return (
-    <div>
+    <Container className="my-4 col-lg-8">
       <Header />
       <FilterablePaginatedPedidoTable />
-    </div>
+    </Container>
   );
 }
