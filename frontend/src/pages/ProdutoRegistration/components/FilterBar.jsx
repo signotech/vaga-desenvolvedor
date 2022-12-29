756436f739712fe5e816894106526141c3ede073
Natã Elienai <nata.elienai@gmail.com>
2022-12-29 12:46:21 -0300

    refactor(front): style produto table filter bar with bootstrap components

--- frontend/src/pages/ProdutoRegistration/components/FilterBar.jsx
+++ frontend/src/pages/ProdutoRegistration/components/FilterBar.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
 
 export default function FilterBar({ onFilterSubmit }) {
   const [filterText, setFilterText] = useState('');
@@ -14,28 +18,43 @@ export default function FilterBar({ onFilterSubmit }) {
   };
 
   return (
-    <form onSubmit={submitFilter}>
-      <input
-        type="search"
-        name="filterText"
-        id="filterText"
-        value={filterText}
-        onChange={(e) => setFilterText(e.target.value)}
-      />
-      <select
-        name="filterField"
-        id="filterField"
-        value={filterField}
-        onChange={(e) => setFilterField(e.target.value)}
-      >
-        <option value="skuProduto">SKU</option>
-        <option value="tituloProduto">Título</option>
-        <option value="preco">Preço</option>
-        <option value="estoque">Estoque</option>
-        <option value="codigoPedido">Código do pedido</option>
-      </select>
-      <button type="submit">Filtrar</button>
-    </form>
+    <Form onSubmit={submitFilter}>
+      <Row className="g-2 align-items-end py-2">
+        <Col xs="auto">
+          <Form.Label htmlFor="filterText" visuallyHidden>
+            Filtro
+          </Form.Label>
+          <Form.Control
+            type="text"
+            name="filterText"
+            id="filterText"
+            placeholder="Filtro"
+            value={filterText}
+            onChange={(e) => setFilterText(e.target.value)}
+          />
+        </Col>
+        <Col xs="auto">
+          <Form.Label htmlFor="filterField" visuallyHidden>
+            Campo
+          </Form.Label>
+          <Form.Select
+            name="filterField"
+            id="filterField"
+            value={filterField}
+            onChange={(e) => setFilterField(e.target.value)}
+          >
+            <option value="skuProduto">SKU</option>
+            <option value="tituloProduto">Título</option>
+            <option value="preco">Preço</option>
+            <option value="estoque">Estoque</option>
+            <option value="codigoPedido">Código do pedido</option>
+          </Form.Select>
+        </Col>
+        <Col xs="auto">
+          <Button type="submit">Filtrar</Button>
+        </Col>
+      </Row>
+    </Form>
   );
 }
 
