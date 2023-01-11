a7424bdf831a3b8b404ac9e1ce3a90152ec3244b
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-01-11 15:51:03 -0300

    Melhora UI de Vaga/Show

--- resources/js/Pages/Vaga/Show.jsx
+++ resources/js/Pages/Vaga/Show.jsx
@@ -1,14 +1,36 @@
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout"
 import { Link } from "@inertiajs/inertia-react"
+import DangerButton from "@/Components/DangerButton"
+import SecondaryButton from "@/Components/SecondaryButton"
 
 export default function Show(props) {
     const vaga = props.vaga
     return (
-        <div className="flex flex-col space-y-1">
-            <div>{vaga.nome}</div>
-            <div>{vaga.descricao}</div>
-            <div>{vaga.tipo}</div>
-            <Link href={route('vagas.edit', vaga.id)}>Editar</Link>
-            <Link method="delete" href={route('vagas.destroy', vaga.id)}>Delete</Link>
-        </div>
+        <AuthenticatedLayout auth={props.auth}>
+            <div className="flex place-content-center">
+                <div className="flex flex-col lg:flex-row p-5 lg:w-3/5 space-y-5">
+                    <div className="flex flex-1 flex-col space-y-5">
+                        <div className="space-x-3">
+                            <Link method="delete" href={route('vagas.destroy', vaga.id)}>
+                                <DangerButton>
+                                    Deletar
+                                </DangerButton>
+                            </Link>
+                            <Link href={route('vagas.edit', vaga.id)}>
+                                <SecondaryButton>
+                                    Editar
+                                </SecondaryButton>
+                            </Link>
+                        </div>
+                        <h1 className="text-3xl">{vaga.nome}</h1>
+                        <hr/>
+                        <div>{vaga.descricao}</div>
+                    </div>
+                    <div className="shadow-inner bg-slate-200 rounded-md h-fit p-5">
+                        Contratação: <div className="inline font-bold">{vaga.tipo}</div>
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
     )
 }
