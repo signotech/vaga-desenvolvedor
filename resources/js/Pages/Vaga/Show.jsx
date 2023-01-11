d4b63089c72e41bb222aafc3cc644ec8f6bae700
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-01-11 11:14:57 -0300

    Adiciona link para edição em Vaga/Show

--- resources/js/Pages/Vaga/Show.jsx
+++ resources/js/Pages/Vaga/Show.jsx
@@ -1,7 +1,10 @@
+import { Link } from "@inertiajs/inertia-react"
+
 export default function Show(props) {
     const vaga = props.vaga
     return (
-        <div className="show">
+        <div className="flex flex-col space-y-1">
+            <Link href={route('vagas.edit', vaga.id)}>Editar</Link>
             <div>{vaga.nome}</div>
             <div>{vaga.descricao}</div>
             <div>{vaga.tipo}</div>
