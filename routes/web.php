69cef53b3ccb856866f5210603452f01a2f43659
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-01-11 10:00:10 -0300

    Adiciona rotas para VagaController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\VagaController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -34,5 +35,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('vagas', VagaController::class);
 
 require __DIR__.'/auth.php';
