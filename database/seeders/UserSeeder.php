ee1db3ee9e91c5c94e0e33c8f846a07cdfb97dd7
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-01-11 18:26:52 -0300

    Altera User seeder para incluir usuario com candidaturas

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\Vaga;
 
 class UserSeeder extends Seeder
 {
@@ -16,5 +17,6 @@ class UserSeeder extends Seeder
     public function run()
     {
         User::factory(10)->create();
+        User::factory()->has(Vaga::factory()->count(3))->create();
     }
 }
