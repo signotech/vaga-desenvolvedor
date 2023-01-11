38872c3ef37722fb0298f7d8a1ba58720f41b32b
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-01-11 11:06:39 -0300

    Implementa Vaga/Create basico

---
+++ resources/js/Pages/Vaga/Create.jsx
@@ -0,0 +1,40 @@
+import PrimaryButton from '@/Components/PrimaryButton'
+import { useForm } from '@inertiajs/inertia-react';
+
+export default function Create(props) {
+    const { data, setData, post, processing } = useForm({
+        nome : '',
+        descricao : '',
+        tipo: 'CLT',
+        user_id : props.auth.user.id
+    });
+
+    const onHandleChange = (event) => {
+        console.log(event.target.value)
+        setData(event.target.name, event.target.type === 'checkbox' ? event.target.checked : event.target.value);
+    };
+
+    const submit = (e) => {
+        e.preventDefault();
+        post(route('vagas.store'));
+    };
+
+    return (
+        <form className="edit flex flex-col p-5 space-y-5" onSubmit={submit}>
+            <label htmlFor="nome">Nome:</label>
+            <input type="text" name="nome" id="nome" value={data.nome} onChange={onHandleChange}/>
+
+            <label htmlFor="descricao">Descrição:</label>
+            <textarea name="descricao" id="descricao" cols="30" rows="10" onChange={onHandleChange} value={data.descricao}/>
+
+            <label htmlFor="tipo">Tipo:</label>
+            <select name="tipo" id="tipo" onChange={onHandleChange}>
+                <option value="CLT">CLT</option>
+                <option value="PJ">PJ</option>
+            </select>
+            <PrimaryButton processing={processing}>
+                Criar
+            </PrimaryButton>
+        </form>
+    );
+}
