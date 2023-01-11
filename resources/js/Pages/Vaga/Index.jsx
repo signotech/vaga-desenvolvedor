6db9ad0be4cfb1d7a8d0e344aed0a1ce9825cff4
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-01-11 11:13:44 -0300

    Adiciona links para visualização e criação em Vagas/Index

--- resources/js/Pages/Vaga/Index.jsx
+++ resources/js/Pages/Vaga/Index.jsx
@@ -1,8 +1,13 @@
+import { Link } from "@inertiajs/inertia-react"
+
 export default function Index(props) {
     const vagas = props.vagas
     return (
-        vagas.map((vaga) => (
-            <div>{vaga.nome}</div>
-        ))
+        <div className="flex flex-col space-y-1">
+            <Link href={route('vagas.create')}>Criar</Link>
+            {vagas.map((vaga) => (
+                <Link href={route('vagas.show', vaga.id)}>{vaga.nome}</Link>
+            ))}
+        </div>
     )
 }
