f18879ed0eee9a291805daa4830695de3e284216
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-01-11 10:00:20 -0300

    Implementa VagaFactory

---
+++ database/factories/VagaFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Vaga>
+ */
+class VagaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'nome' => fake()->catchPhrase(),
+            'descricao' => fake()->text(),
+            'tipo' => 'PJ',
+            'user_id' => 0,
+        ];
+    }
+}
