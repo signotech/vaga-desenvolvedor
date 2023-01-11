365643edd8c13f911e0753619eb6f8d5b06d7c1e
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-01-11 11:36:01 -0300

    Implementa deleção em Vaga/Show

--- resources/js/Pages/Vaga/Show.jsx
+++ resources/js/Pages/Vaga/Show.jsx
@@ -4,10 +4,11 @@ export default function Show(props) {
     const vaga = props.vaga
     return (
         <div className="flex flex-col space-y-1">
-            <Link href={route('vagas.edit', vaga.id)}>Editar</Link>
             <div>{vaga.nome}</div>
             <div>{vaga.descricao}</div>
             <div>{vaga.tipo}</div>
+            <Link href={route('vagas.edit', vaga.id)}>Editar</Link>
+            <Link method="delete" href={route('vagas.destroy', vaga.id)}>Delete</Link>
         </div>
     )
 }
