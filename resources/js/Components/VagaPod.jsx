077f5fdc7088496e7dd95d129476f802110befb9
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-01-11 18:49:44 -0300

    Adiciona botão para incluir candidatura

--- resources/js/Components/VagaPod.jsx
+++ resources/js/Components/VagaPod.jsx
@@ -1,11 +1,24 @@
-import { Link } from "@inertiajs/inertia-react"
+import { Link, useForm } from "@inertiajs/inertia-react"
+import PrimaryButton from "./PrimaryButton";
 
 export default function VagaPod(props) {
     const vaga = props.vaga
+    const { post } = useForm({vaga_id : vaga.id});
+
+    const submit = (e) => {
+        e.preventDefault();
+        post(route('candidaturas.store'));
+    };
+
     return (
-        <Link className="shadow rounded-md border p-5" as='div' href={route('vagas.show', vaga.id)}>
-            <div className="text-xl">{vaga.nome}</div>
-            <div className="truncate w-3/4 text-gray-500">{vaga.descricao}</div>
-        </Link>
+        <form onSubmit={submit}>
+            <Link className="shadow rounded-md border p-5" as='div' href={route('vagas.show', vaga.id)}>
+                <div className="text-xl">{vaga.nome}</div>
+                <div className="truncate w-3/4 text-gray-500">{vaga.descricao}</div>
+                <PrimaryButton className="z-50" onClick={(e) => e.stopPropagation()}>
+                    Candidatar-se
+                </PrimaryButton>
+            </Link>
+        </form>
     )
 }
