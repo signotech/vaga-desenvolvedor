d030204f0c79321727af2afb6bca4376f12aeaab
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-01-11 09:53:52 -0300

    Adiciona Requests de Vaga

---
+++ app/Http/Requests/UpdateVagaRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateVagaRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'nome' => 'required',
+            'descricao' => 'required',
+            'tipo' => 'required',
+            'user_id' => 'required'
+        ];
+    }
+}
