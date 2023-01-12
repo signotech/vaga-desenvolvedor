2cfc6a701372a4de8f5d29b2406d8e7f072b62c2
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-01-12 12:49:00 -0300

    Implementa pausar vagas na UI

--- resources/js/Components/VagaPod.jsx
+++ resources/js/Components/VagaPod.jsx
@@ -1,11 +1,12 @@
 import { Link, useForm } from "@inertiajs/inertia-react"
 import DangerButton from "./DangerButton";
 import PrimaryButton from "./PrimaryButton";
+import { Inertia } from '@inertiajs/inertia'
 
 export default function VagaPod(props) {
     const vaga = props.vaga
     const candidatado = props.candidatado
-    const showButtons = props.showButtons === undefined ? true : props.showButtons
+    const empresa = props.showButtons === undefined ? true : props.showButtons
     const { post, delete : destroy } = useForm({vaga_id : vaga.id});
 
     function onDelete(e) {
@@ -19,21 +20,37 @@ export default function VagaPod(props) {
         post(route('candidaturas.store'))
     }
 
+    function onTogglePausada(e) {
+        e.stopPropagation()
+        vaga.pausada = !vaga.pausada
+        Inertia.put(route('vagas.update', vaga.id), vaga)
+    }
+
     return (
         <Link className="shadow rounded-md border p-5 flex place-content-between space-x-14" as='div' href={route('vagas.show', vaga.id)}>
             <div className="w-3/4">
                 <div className="text-xl">{vaga.nome}</div>
                 <div className="truncate text-gray-500">{vaga.descricao}</div>
             </div>
-            {showButtons && <>
+            {empresa ? <>
                 {candidatado ?
                     <DangerButton className="flex-1 place-content-center" onClick={onDelete}>
                         Cancelar
                     </DangerButton>
-                    :
+                :
                     <PrimaryButton className="flex-1 place-content-center" onClick={onCandidatar}>
                         Candidatar-se
                     </PrimaryButton>
+                }
+            </> : <>
+                {vaga.pausada ?
+                    <PrimaryButton className="flex-1 place-content-center" onClick={onTogglePausada}>
+                        Ativar
+                    </PrimaryButton>
+                :
+                    <DangerButton className="flex-1 place-content-center" onClick={onTogglePausada}>
+                        Pausar
+                    </DangerButton>
                 }
             </>}
         </Link>
