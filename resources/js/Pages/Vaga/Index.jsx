c4bca9a4ffd15aa95111249b88ad4d3d7fd9e3e0
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-01-12 01:58:06 -0300

    Remove botão de adicionar vaga se usuario for candidato

--- resources/js/Pages/Vaga/Index.jsx
+++ resources/js/Pages/Vaga/Index.jsx
@@ -5,15 +5,18 @@ import { Link } from "@inertiajs/inertia-react"
 
 export default function Index(props) {
     const vagas = props.vagas
+    const candidato = props.candidato
     return (
         <AuthenticatedLayout auth={props.auth}>
             <div className="flex flex-col place-items-center p-5">
                 <div className="flex flex-col space-y-3 w-3/5">
-                    <Link as='div' className="active:none sticky top-1" href={route('vagas.create')}>
-                        <PrimaryButton className="w-full place-content-center text-xl">Nova Vaga</PrimaryButton>
-                    </Link>
+                    {!candidato &&
+                        <Link as='div' className="active:none sticky top-1" href={route('vagas.create')}>
+                            <PrimaryButton className="w-full place-content-center text-xl">Nova Vaga</PrimaryButton>
+                        </Link>
+                    }
                     {vagas.map((vaga) => (
-                        <VagaPod candidatado={vaga.candidatado} vaga={vaga}/>
+                        <VagaPod showButtons={candidato} candidatado={vaga.candidatado} vaga={vaga}/>
                     ))}
                 </div>
             </div>
