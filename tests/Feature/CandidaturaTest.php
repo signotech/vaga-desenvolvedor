686e0c5ab6e24a5855684f28e356202d06b13b90
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-01-12 01:45:37 -0300

    Adiciona testes para separação de roles

--- tests/Feature/CandidaturaTest.php
+++ tests/Feature/CandidaturaTest.php
@@ -29,6 +29,15 @@ class CandidaturaTest extends TestCase
         $this->assertDatabaseHas('user_vaga', ['user_id' => $user->id, 'vaga_id' => $vaga->id]);
     }
 
+    public function test_empresa_nao_pode_se_candidatar() {
+        $user = User::factory()->create(['role' => 'empresa']);
+        $vaga = Vaga::factory()->create();
+        $response = $this->actingAs($user)->post('/candidaturas', ['vaga_id' => $vaga->id]);
+
+        $response->assertStatus(403);
+        $this->assertDatabaseMissing('user_vaga', ['user_id' => $user->id, 'vaga_id' => $vaga->id]);
+    }
+
     public function test_candidato_pode_se_descandidatar() {
         $user = User::factory()->has(Vaga::factory()->count(3))->create();
         foreach ($user->vagas as $vaga) {
@@ -37,6 +46,19 @@ class CandidaturaTest extends TestCase
         foreach ($user->vagas as $vaga) {
             $response = $this->actingAs($user)->delete('/candidaturas/'.$vaga->id);
             $this->assertDatabaseMissing('user_vaga', ['user_id' => $user->id, 'vaga_id' => $vaga->id]);
+        }
+    }
+
+    public function test_empresa_nao_pode_se_descandidatar() {
+        $user = User::factory()->has(Vaga::factory()->count(3))->create(['role' => 'empresa']);
+        foreach ($user->vagas as $vaga) {
+            $this->assertDatabaseHas('user_vaga', ['user_id' => $user->id, 'vaga_id' => $vaga->id]);
+        }
+        foreach ($user->vagas as $vaga) {
+            $response = $this->actingAs($user)->delete('/candidaturas/'.$vaga->id);
+
+            $response->assertStatus(403);
+            $this->assertDatabaseHas('user_vaga', ['user_id' => $user->id, 'vaga_id' => $vaga->id]);
         }
     }
 }
--- tests/Feature/VagaTest.php
+++ tests/Feature/VagaTest.php
@@ -22,36 +22,78 @@ class VagaTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function test_tela_de_criacao_de_vagas_renderiza() {
-        $response = $this->get('/vagas/create');
+    public function test_tela_de_criacao_de_vagas_não_renderiza_para_candidatos() {
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->get('/vagas/create');
+        $response->assertStatus(403);
+    }
+
+    public function test_tela_de_criacao_de_vagas_renderiza_para_empresas() {
+        $user = User::factory()->create(['role' => 'empresa']);
+        $response = $this->actingAs($user)->get('/vagas/create');
         $response->assertStatus(200);
     }
 
-    public function test_novas_vagas_podem_ser_criadas(): void
-    {
+    public function test_novas_vagas_não_podem_ser_criadas_por_candidatos() {
         $user = User::factory()->create();
+        $vaga = Vaga::factory()->make();
+        $response = $this->actingAs($user)->post('/vagas', $vaga->toArray());
+
+        $response->assertStatus(403);
+        $this->assertDatabaseMissing('vagas', $vaga->toArray());
+    }
+
+    public function test_tela_de_edicao_de_vagas_não_renderiza_para_candidatos() {
+        $user = User::factory()->create();
+        $vaga = Vaga::factory()->create();
+        $response = $this->actingAs($user)->get('/vagas/'.$vaga->id.'/edit');
+        $response->assertStatus(403);
+    }
+
+    public function test_vagas_não_podem_ser_alteradas_por_candidatos() {
+        $user = User::factory()->create();
+        $vaga = Vaga::factory()->create();
+        $vaga->nome = 'Vaga Teste Editada';
+        $response = $this->actingAs($user)->patch('/vagas/'.$vaga->id, $vaga->toArray());
+
+        $response->assertStatus(403);
+        $this->assertDatabaseMissing('vagas', ['nome' => $vaga->nome]);
+    }
+
+    public function test_vagas_não_podem_ser_deletadas_por_candidatos() {
+        $user = User::factory()->create();
+        $vaga = Vaga::factory()->create();
+        $response = $this->actingAs($user)->delete('/vagas/'.$vaga->id);
+
+        $response->assertStatus(403);
+        $this->assertModelExists($vaga);
+    }
+
+    public function test_novas_vagas_podem_ser_criadas_por_empresas() {
+        $user = User::factory()->create(['role' => 'empresa']);
         $vaga = Vaga::factory()->make();
         $response = $this->actingAs($user)->post('/vagas', $vaga->toArray());
 
         $this->assertDatabaseHas('vagas', $vaga->toArray());
     }
 
-    public function test_tela_de_edicao_de_vagas_renderiza() {
+    public function test_tela_de_edicao_de_vagas_renderiza_para_empresas() {
+        $user = User::factory()->create(['role' => 'empresa']);
         $vaga = Vaga::factory()->create();
-        $response = $this->get('/vagas/'.$vaga->id.'/edit');
+        $response = $this->actingAs($user)->get('/vagas/'.$vaga->id.'/edit');
         $response->assertStatus(200);
     }
 
-    public function test_vagas_podem_ser_alteradas() {
-        $user = User::factory()->create();
+    public function test_vagas_podem_ser_alteradas_por_empresas() {
+        $user = User::factory()->create(['role' => 'empresa']);
         $vaga = Vaga::factory()->create();
         $vaga->nome = 'Vaga Teste Editada';
         $response = $this->actingAs($user)->patch('/vagas/'.$vaga->id, $vaga->toArray());
         $this->assertDatabaseHas('vagas', ['nome' => $vaga->nome]);
     }
 
-    public function test_vagas_podem_ser_deletadas() {
-        $user = User::factory()->create();
+    public function test_vagas_podem_ser_deletadas_por_empresas() {
+        $user = User::factory()->create(['role' => 'empresa']);
         $vaga = Vaga::factory()->create();
         $response = $this->actingAs($user)->delete('/vagas/'.$vaga->id);
         $this->assertModelMissing($vaga);
