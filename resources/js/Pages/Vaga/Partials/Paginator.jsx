9cefacd6c9bebca48978578e012adb7f90a98aef
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-01-13 18:05:10 -0300

    Implementa Paginator

---
+++ resources/js/Pages/Vaga/Partials/Paginator.jsx
@@ -0,0 +1,21 @@
+export default function Paginator(props) {
+    const links = props.links
+    return (
+        <div class="flex justify-center">
+            <nav aria-label="Page navigation example">
+                <ul class="flex list-style-none">
+                    {links.map((link) => (
+                        <li>
+                            <a
+                                className={"block py-1.5 px-3 transition-all duration-300 rounded " + (link.url ? "hover:bg-gray-300 text-gray-800" : "text-gray-500")}
+                                href={link.url}
+                            >
+                                {link.label}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </div>
+    )
+}
