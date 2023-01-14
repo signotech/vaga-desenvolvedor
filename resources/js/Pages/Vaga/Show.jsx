a3c82e50123058240cf6b425bff8bc0da8472fa9
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-01-13 22:47:38 -0300

    Melhora responsividade de Show

--- resources/js/Pages/Vaga/Show.jsx
+++ resources/js/Pages/Vaga/Show.jsx
@@ -11,7 +11,7 @@ export default function Show(props) {
     return (
         <AuthenticatedLayout auth={props.auth}>
             <div className="flex place-content-center">
-                <div className="flex flex-col lg:flex-row p-5 lg:w-3/5 space-y-5 space-x-5">
+                <div className="flex flex-col-reverse lg:flex-row p-5 w-full lg:w-3/5 space-y-reverse space-y-5 lg:space-x-5">
                     <div className="flex flex-1 flex-col space-y-5">
                         <h1 className="text-3xl">{vaga.nome}</h1>
                         <hr/>
@@ -29,23 +29,23 @@ export default function Show(props) {
                             ))}
                         </>}
                     </div>
-                    <div className="shadow-inner bg-slate-200 rounded-md h-fit p-5 space-y-1">
+                    <div className="shadow-inner bg-slate-200 rounded-md w-full lg:w-fit h-fit p-5 space-y-1">
                         Contratação: <div className="inline font-bold">{vaga.tipo}</div>
                         <div className="space-x-3">
                             {candidato ? 
                                 <CandidatarButton vaga={vaga} candidatado={vaga.candidatado} />
-                            : <>
-                                <Link href={route('vagas.edit', vaga.id)}>
-                                    <SecondaryButton>
+                            : <div className="space-x-3 flex">
+                                <Link className="flex-1" href={route('vagas.edit', vaga.id)}>
+                                    <SecondaryButton className="w-full place-content-center">
                                         Editar
                                     </SecondaryButton>
                                 </Link>
-                                <Link method="delete" href={route('vagas.destroy', vaga.id)}>
-                                    <DangerButton>
+                                <Link className="flex-1" method="delete" href={route('vagas.destroy', vaga.id)}>
+                                    <DangerButton className="w-full place-content-center">
                                         Deletar
                                     </DangerButton>
                                 </Link>
-                            </>}
+                            </div>}
                         </div>
                     </div>
                 </div>
