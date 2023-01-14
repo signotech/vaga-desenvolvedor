bcc25c51922a113cc5086e422bff5bcc2b61186e
Ruan Gomes <ruangomesdorosario@gmail.com>
2023-01-14 14:35:29 -0300

    Implementa singleton empresa padrão em VagaFactory

--- database/factories/VagaFactory.php
+++ database/factories/VagaFactory.php
@@ -8,6 +8,7 @@ use App\Models\User;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Vaga>
  */
+
 class VagaFactory extends Factory
 {
     /**
@@ -15,13 +16,23 @@ class VagaFactory extends Factory
      *
      * @return array<string, mixed>
      */
+
+    private static $empresa_padrao_id;
+
+    private static function getEmpresaPadrao() {
+        if (self::$empresa_padrao_id == null) {
+            self::$empresa_padrao_id = User::factory()->create(['role' => 'empresa'])->id;
+        }
+        return self::$empresa_padrao_id;
+    }
+
     public function definition()
     {
         return [
             'nome' => fake()->catchPhrase(),
             'descricao' => fake()->text(),
             'tipo' => 'PJ',
-            'user_id' => User::factory()->create(['role' => 'empresa'])->id,
+            'user_id' => self::getEmpresaPadrao(),
             'pausada' => false,
         ];
     }
