a1ca370890b7c6491e285c68e77d7de60fbc7c7b
Gabriel Stoppa dos Santos <gabrielstoppadossantos@gmail.com>
2023-04-13 15:40:58 -0300

    Criei rotas para o CRUD de clientes

--- api/controllers/customerController.js
+++ api/controllers/customerController.js
@@ -5,5 +5,19 @@ module.exports = {
         const { cpf_cliente, nome_cliente, email_cliente } = req.body;
         const customer = await cliente.create({ cpf_cliente, nome_cliente, email_cliente });
         return res.json(customer);
+    },
+    async getSome(req, res) {
+        const filteredCustomers = await cliente.findAll({where: req.query});
+        res.json(filteredCustomers);
+    },
+    async getOne(req, res) {
+        const { id } = req.params;
+        const singleCustomer = await cliente.findOne({ where: { id }});
+        res.json(singleCustomer);
+    },
+    async deleteOne(req, res) {
+        const { id } = req.params;
+        await cliente.destroy({ where: { id } });
+        res.json({ success: true, deleted: id});
     }
 }
--- api/routes/customerRoutes.js
+++ api/routes/customerRoutes.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
 const customerController = require('../controllers/customerController');
 
-router.post('/cliente', customerController.store);
+
+router.route('/clientes')
+    .get(customerController.getSome)
+    .post(customerController.store);
+
+router.route('/clientes/:id')
+    .get(customerController.getOne)
+    .delete(customerController.deleteOne)
 
 module.exports = router;
