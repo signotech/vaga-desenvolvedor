7dd6fb0b07b2484314c97568e757904845f38a14
tomazcx <tomazcx06@gmail.com>
2023-05-22 10:35:40 -0300

    initial commit: added backend dependencies and folder structure]

---
+++ backend/src/presentation/http/app.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+import cors from 'cors'
+
+const aṕp = express()
+
+aṕp.use(cors())
+
+export default aṕp
+
+
+
---
+++ backend/src/presentation/http/server.ts
@@ -0,0 +1,3 @@
+import app from '@presentation/http/app'
+
+app.listen(3000, () => console.log("Servidor iniciado em localhost:300"))
