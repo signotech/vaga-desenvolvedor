bc4cbc824755f56f960a5458eb54c34b6f871283
kiiinhos <viniciuscamargo713@gmail.com>
2023-05-25 03:28:19 -0300

    colocando tag do swagger auth

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
