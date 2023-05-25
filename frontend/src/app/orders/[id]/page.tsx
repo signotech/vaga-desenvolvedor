6aede6ca573728c8f319d5b0557e13558a414e62
tomazcx <tomazcx06@gmail.com>
2023-05-25 10:15:28 -0300

    feat: finished order page

--- frontend/src/app/orders/[id]/page.tsx
+++ frontend/src/app/orders/[id]/page.tsx
@@ -4,8 +4,11 @@ import { Input } from "@components/atoms/Input"
 import { PageTitle } from "@components/atoms/PageTitle"
 import { SelectStatus } from "@components/atoms/SelectStatus"
 import { DashboardLayout } from "@components/models/DashboardLayout"
+import { ClientItem } from "@components/molecules/ClientItem"
 import { DeleteItemAlert } from "@components/molecules/DeleteItemAlert"
-import { useParams} from "next/navigation"
+import { ProductItem } from "@components/molecules/ProductItem"
+import { PurchasedProduct } from "@components/molecules/PurchasedProduct"
+import { useParams } from "next/navigation"
 import { Calendar, CurrencyDollarSimple, IdentificationCard, Notebook } from "phosphor-react"
 
 const Order = () => {
@@ -16,6 +19,34 @@ const Order = () => {
         <DashboardLayout>
             <section className="lg:col-span-10 flex flex-col p-4 lg:p-16 gap-8">
                 <PageTitle text={`Informações do pedido de id ${params.id}`} />
+                <div className="flex flex-col gap-4">
+                    <h2 className="text-lg lg:text-xl">Dados do pedido:</h2>
+                    <span>Produtos comprados:</span>
+
+                    <div className="flex flex-col gap-2">
+                        <div className="hidden lg:grid grid-cols-12 gap-4 p-2">
+                            <span className="col-span-2">Id</span>
+                            <span className="col-span-3">Título</span>
+                            <span className="col-span-3">SKU</span>
+                            <span className="col-span-2">Preço unitário</span>
+                            <span className="col-span-2">Quantidade</span>
+                        </div>
+                        <PurchasedProduct />
+                    </div>
+
+                    <span>Cliente comprador:</span>
+                    <div className="flex flex-col gap-2">
+                        <div className="hidden lg:grid grid-cols-12 gap-4 p-2">
+                            <span className="col-span-3">Id</span>
+                            <span className="col-span-3">Nome</span>
+                            <span className="col-span-3">Email</span>
+                            <span className="col-span-3">CPF</span>
+                        </div>
+
+                        <ClientItem />
+                    </div>
+
+                </div>
                 <h2 className="text-lg lg:text-xl">Dados cadastrados: </h2>
                 <form className="flex flex-col gap-2" onSubmit={e => e.preventDefault()}>
                     <Input name="id_cliente" id="id_cliente" placeholder="Id do cliente" type="number" ><IdentificationCard className="text-gray-500" size={24} /></Input>
---
+++ frontend/src/components/molecules/PurchasedProduct.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link"
+
+export const PurchasedProduct = () => {
+
+    return (
+        <Link href={`/products/${2}`} className="flex flex-col lg:grid grid-cols-12 gap-4 rounded-lg bg-gray-200 p-2 cursor-pointer transition-colors hover:bg-gray-300">
+            <span className="col-span-2 text-left"><span className="lg:hidden">Id:</span> 1</span>
+            <span className="col-span-3 text-left"><span className="lg:hidden">Título:</span> Carne moída</span>
+            <span className="col-span-3 text-left"><span className="lg:hidden">SKU:</span> SKU81291239</span>
+            <span className="col-span-2 text-left"><span className="lg:hidden">Preço unitário:</span>10.99 </span>
+            <span className="col-span-2 text-left"><span className="lg:hidden">Quantidade:</span>30 </span>
+        </Link>
+
+
+
+    )
+
+}
