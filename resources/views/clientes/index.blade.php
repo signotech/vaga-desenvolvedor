087765edbf84cc0dc22b053bbf5b7ab89c66442c
eduardoranucci <eduardoranucci@outlook.com>
2025-03-29 15:41:01 -0300

    feat: lista todos os clientes

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -10,4 +10,38 @@
 
     <a href="{{ route('clientes.create') }}">Novo</a>
 
+    <br><br>
+
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>E-mail</th>
+                <th>CPF</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+
+            @foreach ($clientes as $cliente)
+                <tr>
+                    <td>{{ $cliente->nome }}</td>
+                    <td>{{ $cliente->email }}</td>
+                    <td>{{ $cliente->cpf }}</td>
+                    <td>
+                        <a href="{{ route('clientes.edit', $cliente->id) }}">
+                            <button>Editar</button>
+                        </a>
+                        <form action="{{ route('clientes.destroy', $cliente->id) }}" method="post" style="display: inline">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit">Excluir</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+
+        </tbody>
+    </table>
+
 @endsection
